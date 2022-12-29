5b47affb4e3582393c272e9f7cf57ec3e2fec84a
Doncard01 <astaciwa578@gmail.com>
2022-12-29 13:04:10 +0100

    pierwsze kroki z instrukcji

---
+++ src/main/java/com/example/AplikacjaBDBT/Application.java
@@ -0,0 +1,13 @@
+package com.example.AplikacjaBDBT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaBdbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacjaBdbtApplication.class, args);
+	}
+
+}

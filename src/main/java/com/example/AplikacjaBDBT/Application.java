26505a368ef2495e657eb42411c1aac93e4bb69f
Doncard01 <astaciwa578@gmail.com>
2022-12-29 13:29:57 +0100

    koniec 1 instrukcji

--- src/main/java/com/example/AplikacjaBDBT/Application.java
+++ src/main/java/com/example/AplikacjaBDBT/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AplikacjaBdbtApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AplikacjaBdbtApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
